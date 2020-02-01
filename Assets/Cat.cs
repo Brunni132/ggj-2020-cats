4f23cac1372bc62ff59f3865ea51f7b1906235d4
Florian <f_bron@hotmail.com>
2020-02-01 16:15:23 +0900

    Mouse laser

--- Assets/Cat.cs
+++ Assets/Cat.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class Cat : MonoBehaviour
 {
-    public GameObject leftCollider, rightCollider, bottomCollider;
+    public const float BLOCK_SIZE = 1;
+    public GameObject leftCollider, rightCollider, bottomCollider, bottomCollider2;
     public SpriteRenderer catSprite;
     public int direction = 1; // initial direction
     public float catVelocity = 1;
@@ -22,9 +25,11 @@ public class Cat : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var pos = transform.position;
-        pos.x += catVelocity * direction * Time.deltaTime;
-        transform.position = pos;
+        // if (collides(bottomCollider) || collides(bottomCollider2) || rigidBody.velocity.y > 0) {
+            var pos = transform.position;
+            pos.x += catVelocity * direction * Time.deltaTime;
+            transform.position = pos;
+        // }
 
         if (collides(leftCollider)) {
             UnityEngine.Debug.LogWarningFormat("TEMP left");
@@ -38,8 +43,12 @@ public class Cat : MonoBehaviour
         catSprite.transform.localScale = new Vector3(direction, 1, 1);
     }
 
-    private bool collides(GameObject obj) {
-        return Physics2D.OverlapCircle(obj.transform.position, 0.01f, wallLayer);
+    // Overrides
+    private void OnCollisionEnter2D(Collision2D other) {
+        var cat = other.gameObject.GetComponent<Cat>();
+        if (cat != null) {
+            cat.collidedWithCat(this);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
@@ -47,5 +56,23 @@ public class Cat : MonoBehaviour
         if (interactable != null) {
             interactable.onCollidedWithCat(this);
         }
+    }
+
+    // Notifications
+    public void laserWasClicked(Vector3 worldPosition)
+    {
+        if (Math.Abs(worldPosition.y - transform.position.y) < BLOCK_SIZE) {
+            direction = worldPosition.x > transform.position.x ? +1 : -1;
+        }
+    }
+
+    public void collidedWithCat(Cat otherCat) {
+        UnityEngine.Debug.LogWarningFormat("TEMP win");
+        SceneManager.LoadScene("GameFinished");
+    }
+
+    // Private
+    private bool collides(GameObject obj) {
+        return Physics2D.OverlapCircle(obj.transform.position, 0.01f, wallLayer);
     }
 }
---
+++ Assets/MouseCursor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCursor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        worldPos.z = transform.position.z;
+        this.transform.position = worldPos;
+
+
+        if (Input.GetMouseButtonDown(0)) {
+            foreach (Cat cat in FindObjectsOfType<Cat>()) {
+                cat.laserWasClicked(worldPos);
+            }
+        }
+    }
+}
