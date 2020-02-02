ada7450938f497a865607f4ab33cf7c16bd862ac
Nilkun <niluxsoftware@gmail.com>
2020-02-02 13:30:53 +0900

    added game over music and images

---
+++ Assets/Scripts/ClickToGoToTitleScreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickToGoToTitleScreen : MonoBehaviour
+{
+	AudioSource sound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sound = GetComponent<AudioSource>();
+		sound.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(Input.GetMouseButtonDown(0)) {
+			SceneManager.LoadScene("TitleScreen");		
+		}
+    }
+}
---
+++ Assets/Scripts/ShowDisclaimer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ShowDisclaimer : MonoBehaviour
+{
+	bool willLoad;
+	IEnumerator goToTitle() {
+		while(true) { 
+			if(willLoad) SceneManager.LoadScene("TitleScreen");
+			willLoad = true;
+			yield return new WaitForSeconds(3);
+		}
+	}
+    // Start is called before the first frame update
+    void Start()
+    {
+		willLoad = false;
+		StartCoroutine(goToTitle());        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
