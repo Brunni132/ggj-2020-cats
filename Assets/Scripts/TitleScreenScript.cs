21450b668afb1c43f1a39e06d0526c1f8fbb3d24
Nilkun <niluxsoftware@gmail.com>
2020-02-02 11:18:30 +0900

    added title screen,probably has music

---
+++ Assets/Scripts/TitleScreenScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenScript : MonoBehaviour
+{
+	AudioSource titleMusic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		titleMusic = GetComponent<AudioSource>();
+		titleMusic.Play();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetMouseButtonDown(0)) {
+		  SceneManager.LoadScene("SampleScene");
+	   } 
+    }
+}
